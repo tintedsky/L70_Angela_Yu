8bf8a6de2fcce827d0072c4591840c9639781767
tintedsky <hongbonew@gmail.com>
2017-11-15 16:56:48 -0500

    external internal parameter examples

--- Contents.swift
+++ Contents.swift
@@ -2,18 +2,21 @@
 
 import UIKit
 
-func getLyrics(numberOfBottlesOfBeer: Int) -> String{
+//forThisManyBottlesOfBeer is the parameter for the caller (external)
+//if external name is replaced by _, the parameter name can be ignored in caller.
+//totalNumberOfBeers is used for the function body (internal)
+func getLyrics(forThisManyBottlesOfBeer totalNumberOfBeers: Int) -> String{
     var lyrics = ""
-    for number in (2...numberOfBottlesOfBeer).reversed(){
+    for number in (2...totalNumberOfBeers).reversed(){
         let newLine = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number - 1) bottles of beer on the wall.\n"
         lyrics += newLine
     }
     
     lyrics += "\n1 bottle of beer on the wall, 1 bottle of beer. \nTake one down and pass it around, no more bottles of beer on the wall.\n"
     
-    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, \(numberOfBottlesOfBeer) bottles of beer on the wall."
+    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, \(totalNumberOfBeers) bottles of beer on the wall."
 
     return lyrics
 }
 
-print(getLyrics(numberOfBottlesOfBeer: 5))
+print(getLyrics(forThisManyBottlesOfBeer:5))
